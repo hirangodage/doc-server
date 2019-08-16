1047889edce4b383cd65c278f7b0105f3b13dd35
hiran@unicorn-solutions.com <hiran@unicorn-solutions.com>
2019-08-16 19:15:14 +0530

    doc fix

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,10 +1,29 @@
 from flask_restplus import Api
 from apis.Documents import documents
 from apis.Health import health
+from flask import url_for
 
+authorizations = {
+    'apikey': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'authorization',
+        'description': 'Enter your bearer token in the format **Bearer &lt;token>**'
+    }
+}
+
+class MyApi(Api):
+    @property
+    def specs_url(self):
+        """Monkey patch for HTTPS"""
+        scheme = 'http' if '8083' in self.base_url else 'https'
+        return url_for(self.endpoint('specs'), _external=True, _scheme=scheme)
 
 api = Api(version='1.0', title='USC API',
     description='use to generate PDF files from HTML',
+    authorizations=authorizations,
+    security='apikey',
+    doc='/'
 )
 
 api.add_namespace(documents)
